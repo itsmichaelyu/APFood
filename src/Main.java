0e6f623b3158be4cf2a0d6cc800f626c0628d5d0
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2021-11-03 22:06:29 -0400

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+//        test
         Food Apple = new Food("Apple", 10, 20.12,true, false, 2000, false);
         Steak Stek = new Steak("Filet Mignon",1, 2.23, true,true, 1000, true, Steak.Rareness.EXTRA_RARE, Steak.Chef.CHEF);
         Cheese Cheddar = new Cheese("Cheddar", 5, 20.93, false,false, 1500, true, false, true);
